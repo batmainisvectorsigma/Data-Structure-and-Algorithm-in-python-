a4071c49a081577563f5d70dd2f4179fcfd23abe
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-09-16 15:29:19 +0545

    Update anagram_detection_checking_off.py

--- AlgorithmAnalysis/anagram_detection_checking_off.py
+++ AlgorithmAnalysis/anagram_detection_checking_off.py
@@ -1,3 +1,39 @@
+#This is the non commented solution
+# If you want to see the commented solution scroll down 
+def anagram_solution_1(s1, s2):
+    still_ok = True
+    if len(s1) != len(s2):
+        still_ok = False
+
+    a_list = list(s2)
+    pos_1 = 0
+
+    while pos_1 < len(s1) and still_ok:
+        pos_2 = 0
+        found = False
+        while pos_2 < len(a_list) and not found:
+            if s1[pos_1] == a_list[pos_2]:
+                found = True
+            else:
+                pos_2 = pos_2 + 1
+        if found:
+            a_list[pos_2] = None
+        else:
+            still_ok = False
+        pos_1 = pos_1 + 1
+
+    return still_ok
+
+
+print(anagram_solution_1("apple", "pleap"))  # expected: True
+print(anagram_solution_1("abcd", "dcba"))  # expected: True
+print(anagram_solution_1("abcd", "dcda"))  # expected: False
+
+
+
+
+
+#This is our commented solution
 def anagram_solution(s1, s2):
     # If lengths of both strings are not same, they can't be anagrams.
     if len(s1) != len(s2):
