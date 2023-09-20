e7927257619b6326a41caf7109de8ab3465728fa
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-09-20 10:44:02 +0545

    Update anagram_detection_count_and_compare.py

--- AlgorithmAnalysis/anagram_detection_count_and_compare.py
+++ AlgorithmAnalysis/anagram_detection_count_and_compare.py
@@ -1,4 +1,36 @@
-"""""""
+"""non commented code"""
+"""COMMENTED CODE IS AT THE BOTTOM"""
+def anagram_solution_4(s1,s2):
+    c1=[0]*26
+    c2=[0]*26
+    
+    for i in range(len(s1)):
+        pos=ord(s1[i])-ord("a")
+        c1[pos]=c1[pos]+1
+    
+    for i in range(len(s2)):
+        pos=ord(s2[i])-ord("a")
+        c2[pos]=c2[pos]+1
+        
+    j=0
+    still_ok=True
+    while j<26 and still_ok:
+        if c1[j] == c2[j]:
+            j=j+1
+        else:
+            still_ok=False
+    return still_ok
+    
+print(anagram_solution_4("apple","pleap"))
+print(anagram_solution_4("abcd","dcba"))
+print(anagram_solution_4("abcd","dcda"))
+
+
+
+
+
+
+
 """Commented program """
 """
 THINGS TO REMEMBER BEFORE LOOKING AT THIS PROGRAM
