a897350f441bc2dc4cf9498ffed2c73ac5807132
whereyoucanfindme <74447376+whocanbevectorsigma@users.noreply.github.com>
2024-01-20 09:50:06 +0545

    Update sequentialsearch.py

--- Searching_and_Sorting/sequentialsearch.py
+++ Searching_and_Sorting/sequentialsearch.py
@@ -31,3 +31,36 @@ case                Best Case         Worst Case            Average Case
 item is present       1                  n                      n/2
 item is not present   n                  n                       n
 """
+
+
+
+
+"""
+    IF THE LIST IS IN ASCENDING ORDER OR IN ANY OTHER WHAT THINGS CAN BE DONE 
+"""
+
+def orderedSequentialSearch(alist, item):
+    pos = 0
+    found = False
+    stop = False
+    
+    while pos < len(alist) and not found and not stop:
+        if alist[pos] == item:
+            found = True
+        else:
+            if alist[pos] > item:
+                stop = True
+            else:
+                pos = pos + 1
+
+    return found
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+print(orderedSequentialSearch(testlist, 3))
+print(orderedSequentialSearch(testlist, 13))
+
+"""
+OUTPUT
+False
+True
+"""
