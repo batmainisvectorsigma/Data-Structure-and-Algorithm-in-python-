a011785487185338e4536e64b6d64fed44cb6f52
whereyoucanfindme <74447376+whocanbevectorsigma@users.noreply.github.com>
2024-01-20 09:46:14 +0545

    Create sequentialsearch.py

---
+++ Searching_and_Sorting/sequentialsearch.py
@@ -0,0 +1,33 @@
+"""
+This is a simple solution to find a item in list
+I will explain it in analysis which is given below"
+"""
+def sequentialSearch(alist, item):
+    pos = 0
+    found = False
+
+    while pos < len(alist) and not found:
+        if alist[pos] == item:
+            found = True
+        else:
+            pos = pos + 1
+    return found
+
+testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+print(sequentialSearch(testlist, 3))
+print(sequentialSearch(testlist, 13))
+
+"""
+OUTPUT
+False
+True
+"""
+
+
+"""
+     ALGORITHM ANALYSIS
+complexity is O(n)
+case                Best Case         Worst Case            Average Case
+item is present       1                  n                      n/2
+item is not present   n                  n                       n
+"""
