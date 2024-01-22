61585b9855571fd4f4372f9b95a58a4e0548c96e
whereyoucanfindme <74447376+whocanbevectorsigma@users.noreply.github.com>
2024-01-22 11:02:15 +0545

    Create hashing.py

---
+++ Searching_and_Sorting/hashing.py
@@ -0,0 +1,10 @@
+def hash(astring, tablesize):
+    sum = 0
+    for pos in range(len(astring)):
+        sum = sum + ord(astring[pos])
+
+    return sum%tablesize
+
+print(hash("cat",11))
+#output
+# 4
